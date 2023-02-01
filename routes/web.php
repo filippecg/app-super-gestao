ad6393a6e9d2c2b31a3a2288349fc5f807921b35
Filippe Gomes <filippecg88@gmail.com>
2023-02-01 19:11:16 +0000

    Criando componente para formulario de contato e criando conexao com o banco de dados.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/login', function() { return 'Login'; })->name('site.login');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function() { return 'clientes'; })->name('app.clientes');
